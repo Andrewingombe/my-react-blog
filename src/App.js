5c442a22ba7d77244155f2432810db2b8e498edc
Andrew Ingombe <ingombe.ani@gmail.com>
2021-11-19 07:11:45 +0200

    Finish setting up routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import Header from "./Header";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Missing from "./Missing";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Header />
+      <Nav />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/post" exact element={<NewPost />} />
+        <Route path="/post/:id" exact element={<PostPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
