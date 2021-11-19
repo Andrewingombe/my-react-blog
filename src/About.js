b7edc50ea5a6f40ec249c4a76bd644fd7d346037
Andrew Ingombe <ingombe.ani@gmail.com>
2021-11-19 09:51:32 +0200

    Finish routing and http methods

--- src/About.js
+++ src/About.js
@@ -2,8 +2,14 @@ import React from "react";
 
 const About = () => {
   return (
-    <main>
+    <main className="About">
       <h1>About</h1>
+      <p style={{ marginTop: "1rem" }}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil
+        repellendus, molestiae distinctio, est sed dicta deserunt deleniti,
+        inventore similique obcaecati facere mollitia aspernatur unde. Ipsam
+        architecto aliquam fugit suscipit recusandae.
+      </p>
     </main>
   );
 };
--- src/App.js
+++ src/App.js
@@ -8,16 +8,73 @@ import About from "./About";
 import Missing from "./Missing";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { format } from "date-fns";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const filteredResults = posts.filter(
+      (post) =>
+        post.body.toLowerCase().includes(search.toLowerCase()) ||
+        post.title.toLowerCase().includes(search.toLowerCase())
+    );
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const newPost = {
+      id,
+      title: postTitle,
+      datetime,
+      body: postBody,
+    };
+    const allPosts = [...posts, newPost];
+    setPosts(allPosts);
+    setPostTitle("");
+    setPostBody("");
+    navigate("/");
+  };
+
+  const handleDelete = (id) => {
+    const postList = posts.filter((post) => post.id !== id);
+    setPosts(postList);
+    navigate("/");
+  };
+
   return (
     <div className="App">
-      <Header />
-      <Nav />
+      <Header title="React JS Blog" />
+      <Nav search={search} setSearch={setSearch} />
       <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/post" exact element={<NewPost />} />
-        <Route path="/post/:id" exact element={<PostPage />} />
+        <Route path="/" exact element={<Home posts={searchResults} />} />
+        <Route
+          path="/post"
+          exact
+          element={
+            <NewPost
+              handleSubmit={handleSubmit}
+              postTitle={postTitle}
+              setPostTitle={setPostTitle}
+              postBody={postBody}
+              setPostBody={setPostBody}
+            />
+          }
+        />
+        <Route
+          path="/post/:id"
+          exact
+          element={<PostPage posts={posts} handleDelete={handleDelete} />}
+        />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<Missing />} />
       </Routes>
